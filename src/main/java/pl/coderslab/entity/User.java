4dad2f5999c4666ab7d3a8dfcc60bc9af960629e
seblas <sebastian.lasek77@gmail.com>
2024-02-17 17:59:33 +0100

    Klasa User utworzona

---
+++ src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,51 @@
+package pl.coderslab.entity;
+
+public class User {
+    private static int id;
+    private static String userName;
+    private static String email;
+    private static String password;
+
+    public User() {
+        this.id = 0;
+    }
+
+    public User(String userName, String email, String password) {
+        this.id = 0;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        User.id = id;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        User.userName = userName;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        User.email = email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+}
